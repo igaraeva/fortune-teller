f41440029b700d295ea1db213eec99c342757b59
igaraeva <161602447+igaraeva@users.noreply.github.com>
2024-05-17 22:26:07 +0100

    further prompt engineering

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ function generateFortune(event) {
   event.preventDefault();
 
   let dateOfBirth = document.querySelector("#date-of-birth");
-  let prompt = `Generate a personal future prediction based on a person's date of birth: ${dateOfBirth.value}. The prediction should be short, one sentence long with NO headings or subheadings. You should avoid stating the current year. If ${dateOfBirth.value} does not have any numbers, prompt the user to input their date of birth in the following format: DD/MM/YYYY`;
+  let prompt = `Generate a personal future prediction based on a person's date of birth: ${dateOfBirth.value}. The prediction should be short, one sentence long with NO headings or subheadings. You should avoid stating the current year. The user's date of birth (${dateOfBirth.value}) should have a full date (day, month, year), if not - prompt the user to input their date of birth in this format: DD/MM/YYYY`;
   let context =
     "You are an AI assistant who loves astrology, numerology and zodiac signs. You can give future predictions based on a person's date of birth";
   let apiKey = "51f9883450a0f1682ta31o8477291fb1";
